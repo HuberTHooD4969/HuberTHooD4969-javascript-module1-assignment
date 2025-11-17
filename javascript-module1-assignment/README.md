7151b0f6bc4dfe31c0440dcbf5d22a5041b53314
HuberTHooD4969 <130329245+HuberTHooD4969@users.noreply.github.com>
2025-11-17 21:14:27 +0000

    Add files via upload

---
+++ javascript-module1-assignment/README.md
@@ -0,0 +1,12 @@
+ Module 1 Assignment – Introduction to JavaScript
+
+1. C — Adding interactivity and logic to web pages  
+
+2. C — <script src="script.js"></script>  
+
+3. C — At the bottom of the <body>  
+
+4. C — Hello World  
+
+5. B — Displays a popup box with a message
+
---
+++ javascript-module1-assignment/script.js
@@ -0,0 +1,12 @@
+
+alert("Welcome to my website!");
+
+
+function changeText() {
+  document.getElementById("info").textContent = "You just clicked the button!";
+}
+
+console.log("Name: Hubert Kwabena Afriyie");
+console.log("Favorite Color: Violet");
+console.log("Current Year: " + new Date().getFullYear());
+
